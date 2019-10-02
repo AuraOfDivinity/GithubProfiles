74f3a8f6c84d51661ae3da44a8e8edbddc80fb1b
Auraofdivinity <peirisasel373@gmail.com>
2019-10-02 22:20:09 +0530

    Updates

---
+++ precache-manifest.d77b60fc5a1df7a849c7b6bb1f1d1169.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "824e2d15f692c26d4b1ed2769fc0ea72",
+    "url": "/repository-name/index.html"
+  },
+  {
+    "revision": "20ca36ea49b82cdbcbb9",
+    "url": "/repository-name/static/css/2.3aff398d.chunk.css"
+  },
+  {
+    "revision": "70f3ac2bb9b5cce945d2",
+    "url": "/repository-name/static/css/main.075d9a29.chunk.css"
+  },
+  {
+    "revision": "20ca36ea49b82cdbcbb9",
+    "url": "/repository-name/static/js/2.bf2b05f1.chunk.js"
+  },
+  {
+    "revision": "70f3ac2bb9b5cce945d2",
+    "url": "/repository-name/static/js/main.bb5910bb.chunk.js"
+  },
+  {
+    "revision": "5fd4fba3d5199690787b",
+    "url": "/repository-name/static/js/runtime-main.5f49de2e.js"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/repository-name/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/repository-name/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/repository-name/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/repository-name/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/repository-name/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/repository-name/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/repository-name/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/repository-name/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/repository-name/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/repository-name/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/repository-name/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/repository-name/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/repository-name/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/repository-name/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/repository-name/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/repository-name/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/repository-name/static/media/outline-icons.ef60a4f6.woff"
+  }
+]);
